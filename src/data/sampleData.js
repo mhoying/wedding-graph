dc7a0fa022a6e6d43fb021fe055f524d989cad9c
mhoying <matt.hoying@gmail.com>
2026-09-06 22:28:56 -0700

    fix(headshot): update jess_phan headshot & deployment

--- src/data/sampleData.js
+++ src/data/sampleData.js
@@ -1,4 +1,4 @@
-export const BUILD_TIMESTAMP = 1788758806084;
+export const BUILD_TIMESTAMP = 1788758932526;
 // Real Wedding Guest List Data - Auto-updated via Host Admin Suite
 export const COHORT_COLORS = {
   "The Couple": "#38bdf8",
