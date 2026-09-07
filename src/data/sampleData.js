6a49caa2081b6963e4bd560807ef7f2eba436b2a
mhoying <matt.hoying@gmail.com>
2026-09-06 22:26:50 -0700

    fix(headshot): update mark_macdonald headshot & deployment

--- src/data/sampleData.js
+++ src/data/sampleData.js
@@ -1,4 +1,4 @@
-export const BUILD_TIMESTAMP = 1788758649570;
+export const BUILD_TIMESTAMP = 1788758806084;
 // Real Wedding Guest List Data - Auto-updated via Host Admin Suite
 export const COHORT_COLORS = {
   "The Couple": "#38bdf8",
