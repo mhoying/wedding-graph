7b9cd12d5551fa9849fa33da79683f9a5d24b25c
mhoying <20133439+mhoying@users.noreply.github.com>
2026-09-08 14:36:00 +0000

    Auto-approve guest proposal #77 [skip ci]

--- src/data/sampleData.js
+++ src/data/sampleData.js
@@ -526,7 +526,10 @@ export const SAMPLE_NODES = [
     "hobbies": [
       "Tennis",
       "Cycling",
-      "Baking"
+      "Baking",
+      "Running",
+      "Movies",
+      "Cooking"
     ],
     "__indexColor": "#700014",
     "x": 559.0993679752828,
